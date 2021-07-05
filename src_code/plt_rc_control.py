5148857f6749fdc3af99644b2500db24416214af
Kerryn <34811018+elevsev@users.noreply.github.com>
2021-07-05 16:03:33 +0100

    Create plt_rc_control.py

---
+++ src_code/plt_rc_control.py
@@ -0,0 +1,6 @@
+plt.rc('font', size=25) #controls default text size
+plt.rc('axes', titlesize=25) #fontsize of the title
+plt.rc('axes', labelsize=15) #fontsize of the x and y labels
+plt.rc('xtick', labelsize=15) #fontsize of the x tick labels
+plt.rc('ytick', labelsize=15) #fontsize of the y tick labels
+plt.rc('legend', fontsize=15) #fontsize of the legend
